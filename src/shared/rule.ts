71848cd71df8f0b96788da4d6842842f9eacf49c
sunhwa <2017sunhwa@gmail.com>
2021-10-08 19:13:00 +0900

    ✨[feat] .env 환경변수 추가

---
+++ src/shared/rule.ts
@@ -0,0 +1,11 @@
+/**
+ * 입력한 문제중 태그로 인식되는 것들은 문자로 변환
+ * @param str 문자열
+ */
+export const ConvertQuestion = (str: string): string => {
+  str = str.replace(/&lt;/g, '<');
+  str = str.replace(/&gt;/g, '>');
+  str = str.replace(/&quot;/g, '"');
+  str = str.replace(/&#039;/g, "'");
+  return str;
+};
